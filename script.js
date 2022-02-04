4ced08ba8a40199fe808add12ae8f1e40ce05405
raito417 <coracola.1020@gmail.com>
2022-02-04 23:28:12 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         mute-wordle-mastodon
+// @version      0.1
+// @description  mute wordle
+// @author       raito@mistodon.cloud
+// @match        https://mistodon.cloud/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @require      https://code.jquery.com/jquery-3.3.1.js
+//
+// 以下のサイトを参考にさせてもらいました。コードをパクらせていただきました。感謝します。
+// https://qiita.com/BlueRayi/items/2effe1b7f9784eef249f
+//
+// ==/UserScript==
+
+/* globals $ */
+
+(function() {
+    'use strict';
+    console.log('start')
+    /* ミュートしたい単語のリスト */
+    const wordmute_list = [
+        ':large_green_square:',
+        ':large_yellow_square:',
+        ':black_large_square:',
+    ];
+    const observer = new MutationObserver(records => {
+        wordmute_list.forEach(element => {
+            $('div.status__content__text').find(`img[title="${element}"]`).closest('p').replaceWith('Auto CW wordleがこの投稿をミュートしました').removeClass();
+        });
+    });
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+})();
